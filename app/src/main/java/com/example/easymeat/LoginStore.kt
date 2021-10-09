400da8d1ff60037adc2275ca725593a697711345
jjalf <jjalfredo68@gmail.com>
2021-10-09 16:41:52 -0500

    Añadir productos

--- app/src/main/java/com/example/easymeat/LoginStore.kt
+++ app/src/main/java/com/example/easymeat/LoginStore.kt
@@ -7,5 +7,10 @@ class LoginStore : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_store)
+        <Button
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="@string/button_text"
+        ... />
     }
 }
